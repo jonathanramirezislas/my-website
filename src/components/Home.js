86bb016b8b26b80d4b4e5ada036bf7d80164bb9e
Jonathan Ramirez <68352096+jonathanramirezislas@users.noreply.github.com>
2021-03-02 12:01:29 -0600

    useWindowsSize

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,14 @@
-import React from 'react'
+import React,{useEffect} from 'react'
 import Navbar from './ui/Nabvar'
 import { Banner } from './ui/Banner';
 import Stack from './ui/Stack';
 import { About } from './ui/About';
 import { Projects } from './ui/Projects';
 import { motion } from "framer-motion"
+import { useWindowSize } from './../customehooks/UseWindosSize';
 
 export const Home = () => {
+   
     return (
         <div className="w-full h-screen bg-black">
             <Navbar/>
--- src/components/ui/Project.js
+++ src/components/ui/Project.js
@@ -1,27 +1,48 @@
-import React, {useEffect} from 'react';
+import React, { useEffect } from 'react';
 import { motion } from 'framer-motion';
 import { useAnimation } from 'framer-motion';
 import { useInView } from 'react-intersection-observer';
-
-const BoxVariants = {
-	visible: { opacity: 1, x: 0, transition: { duration: 1 } },
-	hidden: { opacity: 0, x: 60 }
-};
+import { useWindowSize } from '../../customehooks/UseWindosSize';
 
 export const Project = () => {
+	const {width, height} = useWindowSize();
+	const x= width>1000 ? 350 : width>800 ? 200 : 60;
+	
 	const controls = useAnimation();
 	const [ref, inView] = useInView();
 
+	console.log('x:',x ,'width',width);
+
+	const BoxVariants = {
+		visible: { opacity: 1, x: 0, transition: { duration: 1 } },
+		hidden: { opacity: 0, x: x },
+	};
+
+
+
 	useEffect(() => {
-		if (inView) {
+		if (inView ) {
 			controls.start('visible');
 		}
 	}, [controls, inView]);
 
-	return <motion.div  
-    ref={ref} 
-	className="w-10/12 h-40 mt-24 bg-red-600 rounded"
-    animate={controls}
-    initial="hidden" 
-    variants={BoxVariants}><div>sss</div></motion.div>
+	 if(width){
+	return (
+		<motion.div
+			ref={ref}
+			className="w-10/12 h-40 mt-24 bg-red-600 rounded"
+			animate={controls}
+			initial="hidden"
+			variants={BoxVariants}
+		>
+			<div>
+				w-{width}px / h-{height}px
+			</div>
+		</motion.div>
+	)}else{
+		return(
+			<div></div>
+		)
+	}
+	
 };
